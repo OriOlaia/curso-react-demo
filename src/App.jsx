462560f6d5ab94244bb97d58d2f6a0365f988422
Oriana Olaia <oriana.olaia17@gmail.com>
2024-04-11 14:38:19 -0300

    agregando más cositas

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+export function App (){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/deviantart/spyed" alt="Avatar de Spyed" />
+                <div>
+                    <strong>Spyed</strong>
+                    <span>@spyed</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
